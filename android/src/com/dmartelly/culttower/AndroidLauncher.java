23d042333cd9cffd19dd89be6fac36410ce05afa
Dominick Martelly <dominick.martelly@gmail.com>
2017-04-01 14:59:00 -0400

    Working on Cult Tower again

---
+++ android/src/com/dmartelly/culttower/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.dmartelly.culttower;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.dmartelly.culttower.CultTower;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CultTower(), config);
+	}
+}
